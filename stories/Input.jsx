4b517cd62aade373fbc40cd48087821a1eb94899
Kurnianto Syaputra <kurnianto.syaputra@spesolution.net>
2022-10-12 09:34:47 +0700

    feature/tambah-story

--- stories/Input.jsx
+++ stories/Input.jsx
@@ -2,19 +2,27 @@
 
 import React from 'react';
 import styles from "./Input.module.css"
-export const InputModif = ({ label, width="120",borderRadius, fontColor,type="text" ,...props }) => {
+import PropTypes from 'prop-types';
+export const InputModif = ({ label, width, fontColor,type="text" ,...props }) => {
     
     const style = {
         color : fontColor,
-        borderRadius:`${borderRadius || 20}px`
     }
   
     return (
-    <div class="form__group" style={{width:`${width}px`}}>
+    <div class="form__group" style={{width:`${width ? `${width}px` : 'auto'}`}}>
         <input type={type} style={style} class={styles["form__input"]} id="name" placeholder={label} {...props} />
         <label for="name" class={styles["form__label"]}>{label}</label>
     </div>
   );
 };
 
+
+// InputModif.propTypes = {
+//     width: PropTypes.number,
+//     label: PropTypes.string,
+//     borderRadius : PropTypes.number,
+//     type: PropTypes.oneOf(["number","text","password"]),
+//     color: PropTypes.string
+//   };
 
--- stories/Input.stories.jsx
+++ stories/Input.stories.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import {InputModif} from "./Input"
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
     title: 'InputModif',
     component: InputModif,
     argTypes: {
         fontColor: { control: 'color' },
       },
+      
   }
 
 const Template = (args) => <InputModif {...args}/> 
-
+export const Input = Template.bind({})
 export const Number = Template.bind({})
+export const Password = Template.bind({})
+
+Input.args = {
+    label : "Ini Input",
+
+}
 
 Number.args = {
     label : "Ini Label Number",
     type  : "number",
     width: 100,
-    borderRadius:10
 }
 
+Password.args = {
+    label : "Ini Label Password",
+    type  : "password",
+    width: 100,
+}
